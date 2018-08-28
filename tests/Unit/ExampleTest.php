d4a557c7d51eec682d89f899bf4efef328836b32
Vincent Gabriel <vadimg88@gmail.com>
2018-08-28 09:40:30 -0700

    updated unittests

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -4,6 +4,7 @@ namespace Tests\Unit;
 
 use FHALicenses\Licenses;
 use PHPUnit\Framework\TestCase;
+use TheIconic\NameParser\Parser as NameParser;
 
 class ExampleTest extends TestCase
 {
@@ -12,6 +13,33 @@ class ExampleTest extends TestCase
     {
         $records = (new Licenses())->setState('AK')->all();
         
-        print_r($records);
+        $this->assertNotEmpty($records);
+    }
+
+    /** @test */
+    public function it_can_parse_saluation_at_end()
+    {
+        $parsed = (new NameParser)->parse('PAUL M LEWIS MR');
+        
+        $this->assertEquals('Paul', $parsed->getFirstName());
+        $this->assertEquals('Lewis', $parsed->getLastName());
+    }
+
+    /** @test */
+    public function it_can_parse_saluation_lastname()
+    {
+        $parsed = (new NameParser)->parse('SUJAN MASTER');
+
+        $this->assertEquals('Sujan', $parsed->getFirstName());
+        $this->assertEquals('Master', $parsed->getLastName());
+    }
+
+    /** @test */
+    public function it_can_parse_saluation_lastname_with_middlename()
+    {
+        $parsed = (new NameParser)->parse('JAMES J MA');
+        
+        $this->assertEquals('James', $parsed->getFirstName());
+        $this->assertEquals('Ma', $parsed->getLastName());
     }
 }
